46b996664246074086a57bcbf3d758188ed99d55
kolik <kolik@6780152c-57a2-4364-a193-6a9102f7105b>
2007-08-28 23:45:44 +0000

    Poladění dataview interfacu a jeho implementačních tříd, pokračování práce na testovací třídě pro info dialogy

--- moriaengine/scripts/systems/dialogs/ImprovedDialog.cs
+++ moriaengine/scripts/systems/dialogs/ImprovedDialog.cs
@@ -328,6 +328,28 @@ namespace SteamEngine.CompiledScripts.Dialogs {
 			}
 			return pagingHandled;
 		}
+
+		[Remark("Write a single DataField to the dialog")]
+		public static void WriteDataField(IDataFieldView field, object target, GUTAComponent where, params int[] index) {
+			if (field.IsButtonEnabled) { //buttonized field - we need the button index
+				if (index == null || index.Length == 0) {
+					throw new SEException(LogStr.Error("Trying to write a " + field.GetType() + " for object " + target + " but missing the button dialog index"));
+				}
+				where.AddComponent(ButtonFactory.CreateButton(LeafComponentTypes.ButtonTick,index[0]));
+				where.AddComponent(TextFactory.CreateLabel(ButtonFactory.D_BUTTON_WIDTH,0,field.Name));
+			} else if (!field.ReadOnly) { //editable label-value field - we still need the index !
+				if (index == null || index.Length == 0) {//we have a problem - we need the index !
+					throw new SEException(LogStr.Error("Trying to write a " + field.GetType() + " for object " + target + " but missing the edit field dialog index"));			
+				}
+				where.AddComponent(TextFactory.CreateLabel(field.Name));
+				//insert the input field - specify the x and y position, let the engine to compute the width and height of the component
+				where.AddComponent(InputFactory.CreateInput(LeafComponentTypes.InputText, field.GetStringValue(target), ImprovedDialog.INPUT_INDENT, 0, index[0]));
+			} else { //non-editable label-value field
+				where.AddComponent(TextFactory.CreateLabel(field.Name));
+				where.AddComponent(TextFactory.CreateText(ImprovedDialog.INPUT_INDENT, 0, field.GetStringValue(target)));
+			}			
+		}
+
 		[Remark("Dialog constants")]
 		public const int D_DEFAULT_DIALOG_BORDERS = 9250; //grey borders
 		public const int D_DEFAULT_DIALOG_BACKGROUND = 9354; //beige background
--- moriaengine/scripts/systems/nastaveni/Settings.cs
+++ moriaengine/scripts/systems/nastaveni/Settings.cs
@@ -22,7 +22,7 @@ using SteamEngine.Common;
 namespace SteamEngine.CompiledScripts.Dialogs {
 	[Remark("Interface for displaying the labels and values of single members of the target"+
 			"(infoized) object in the dialog")]
-	interface IDataFieldView {
+	public interface IDataFieldView {
 		[Remark("The name of this field / the label of the button")]
 		string Name { get; }
 		
@@ -33,127 +33,128 @@ namespace SteamEngine.CompiledScripts.Dialogs {
 		bool IsButtonEnabled { get; }
 
 		[Remark("Take the target object and retreive its member's (for which this interface instance is) value")]
-		string GetValue(object target);
+		object GetValue(object target);
 
 		[Remark("Take the target object and set its member's value")]
-		void SetValue(object target, object value);
+		bool SetValue(object target, object value);
+
+		[Remark("Take the target object and retreive its member's value in the stringified form")]
+		string GetStringValue(object target);
+
+		[Remark("Take the stringified value, convert it and set it to the respective member of the target")]
+		bool SetStringValue(object target, string value);
 
 		[Remark("What will happen when the button is pressed?")]
-		void OnButton(object target);
-
-		[Remark("Provides the creation of a dialog field for a particular member of 'target', "+
-				"it will be placed to 'where', the button's or editable field's index is the first value in "+
-				"'index' params array"+
-				"If the index is used for the non-editable or non-button enabled dataview field, an exception"+
-				"will be thrown")]
-		void Write(object target, GUTAComponent where, params int[] index);		
-	}
-
-	[Remark("Abstract class providing basic implementation of IDataFieldView")]
-	public abstract class AbstractDataFieldView : IDataFieldView {
-		//private MemberInfo member; //can be a field or method
-
-		//[Remark("Setting the MemberInfo field - we will examine the exact type of the member"+
-		//		"and make further settings")]
-		//public MemberInfo Member {
-		//	set {
-		//		member = value;
-		//		if(member is MethodBase) {
-		//			//the member is a method - we will need a button here !
-		//			buttonized = true;
-		//		} else if(member is FieldInfo) {
-		//			//it will be the label-value field...
-		//			buttonized = false;
-		//		}
-		//	}
-		//}
-
-		//The properties will be set properly in the child classes
-		public abstract bool ReadOnly { get; }
-		public abstract bool IsButtonEnabled { get; }
-		public abstract string Name { get; }
-
-		//We will leave the implementation of these methods to children of this class
-		public abstract string GetValue(object target);
-		public abstract void SetValue(object target, object value);
-		public abstract void OnButton(object target);
-		public abstract void Write(object target, GUTAComponent where, params int[] index);
+		void OnButton(object target);				
 	}
 
 	[Remark("Abstract class providing basics to display a non editable 'label-value' in the dialog")]
-	public abstract class ReadOnlyDataFieldView : AbstractDataFieldView {
+	public abstract class ReadOnlyDataFieldView : IDataFieldView {
 		[Remark("There is no button for this dataview field")]
-		public override bool IsButtonEnabled {
+		public bool IsButtonEnabled {
 			get {
 				return false;
 			}			
 		}
 
 		[Remark("Yes, this dataview field is read only")]
-		public override bool ReadOnly {
+		public bool ReadOnly {
 			get {
 				return true;
 			}
 		}
 
 		[Remark("This method is forbidden in this class")]
-		public override void SetValue(object target, object value) {
+		public bool SetValue(object target, object value) {
 			throw new SEException(LogStr.Error("Cannot set a value to the non-editable field"));
 		}
 
-		[Remark("This field does not have any buttons too - buttons has another type of data view")]
-		public override void OnButton(object target) {
+		[Remark("This method is forbidden in this class")]
+		public bool SetStringValue(object target, string value) {
+			throw new SEException(LogStr.Error("Cannot convert and set a stringified value to the non-editable field"));
+		}
+
+		[Remark("This field does not have any buttons too - buttons have another type of data view")]
+		public void OnButton(object target) {
 			throw new SEException(LogStr.Error("This dataview cannot have any buttons"));
 		}
+
+		//all other properties/methods will be implemented in child classes later
+		public abstract string Name { get; }
+		public abstract object GetValue(object target);
+		public abstract string GetStringValue(object target);
 	}
 
 	[Remark("Abstract class providing basics to display an editable 'label-value' in the dialog")]
-	public abstract class ReadWriteDataFieldView : AbstractDataFieldView {
+	public abstract class ReadWriteDataFieldView : IDataFieldView {
 		[Remark("There is no button for this dataview field")]
-		public override bool IsButtonEnabled {
+		public bool IsButtonEnabled {
 			get {
 				return false;
 			}
 		}
 
 		[Remark("No, this dataview field can be edited")]
-		public override bool ReadOnly {
+		public bool ReadOnly {
 			get {
 				return false;
 			}
 		}
 
 		[Remark("This field does not have any buttons - buttons are present in another type of data view")]
-		public override void OnButton(object target) {
+		public void OnButton(object target) {
 			throw new SEException(LogStr.Error("This dataview cannot have any buttons"));
 		}
+
+		//all other properties/methods will be implemented in child classes later
+		public abstract string Name { get; }
+		public abstract object GetValue(object target);
+		public abstract bool SetValue(object target, object value);
+		public abstract string GetStringValue(object target);
+		public abstract bool SetStringValue(object target, string value);
+
 	}
 
 	[Remark("Abstract class providing basics to display a '[button]-label' in the dialog")]
-	public abstract class ButtonDataFieldView : AbstractDataFieldView {
+	public abstract class ButtonDataFieldView : IDataFieldView {
 		[Remark("There is actually a button present for this dataview field")]
-		public override bool IsButtonEnabled {
+		public bool IsButtonEnabled {
 			get {
 				return true;
 			}
 		}
 
 		[Remark("Nothing to write or set here...")]
-		public override bool ReadOnly {
+		public bool ReadOnly {
 			get {
 				return true;
 			}
 		}
 
 		[Remark("This method is forbidden in this class, there is nothing to set")]
-		public override void SetValue(object target, object value) {
+		public bool SetValue(object target, object value) {
 			throw new SEException(LogStr.Error("Cannot set any value to a buttonized dataview field"));
 		}
 
 		[Remark("This method is forbidden in this class, there is nothing to get")]
-		public override string GetValue(object target) {
+		public object GetValue(object target) {
 			throw new SEException(LogStr.Error("Cannot get any value from the buttonized dataview field"));
 		}
+
+		[Remark("This method is forbidden in this class, there is nothing to set")]
+		public bool SetStringValue(object target, string value) {
+			throw new SEException(LogStr.Error("Cannot convert and set any stringified value to a buttonized dataview field"));
+		}
+
+		[Remark("This method is forbidden in this class, there is nothing to get")]
+		public string GetStringValue(object target) {
+			throw new SEException(LogStr.Error("Cannot get and convert any value from the buttonized dataview field"));
+		}
+
+		//all other properties/methods will be implemented in child classes later
+		public abstract string Name { get; }
+		public abstract void OnButton(object target);
+
 	}
 
 	[Summary("Decorate your class by this attribute if you want it to be viewable by info dialogs.")]
@@ -162,6 +163,15 @@ namespace SteamEngine.CompiledScripts.Dialogs {
 		//no params constructor
 		public ViewableClassAttribute() {
 		}		
+	}
+
+	[Summary("Decorate amember of the ViewableClass by this attribute if you want to prevent them to be displayed in info dialogs."+
+			 "all other attributes will be displayed")]
+	[AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+	public class NoShowAttribute : Attribute {
+		//no params constructor
+		public NoShowAttribute() {
+		}
 	}
 
 	[Summary("Used in ViewableClasses for methods we want to be available as buttons in the info dialogs.")]
