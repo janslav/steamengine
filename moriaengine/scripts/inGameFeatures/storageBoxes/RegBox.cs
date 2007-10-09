fd27a377bd9dc3809a406038b2c4f7c88b7600a1
glorg <glorg@6780152c-57a2-4364-a193-6a9102f7105b>
2007-10-09 19:49:50 +0000

    RegBox - hopefully working version.

--- moriaengine/scripts/inGameFeatures/storageBoxes/RegBox.cs
+++ moriaengine/scripts/inGameFeatures/storageBoxes/RegBox.cs
@@ -36,13 +36,12 @@ namespace SteamEngine.CompiledScripts {
 
 		public override void On_DClick(AbstractCharacter ac) {
 			Character dClicker = ac as Character;
-			if (!dClicker.GetMap().CanSeeLOSFromTo(dClicker, this)) {
-				ac.SysMessage("ted bys na to hele jako ze nemel videt");
+			if (dClicker.CanReach(this)) {
+				if (dClicker.currentSkill != null) {
+					dClicker.AbortSkill();
+				}
+				this.Dialog(ac, SingletonScript<Dialogs.D_RegBox>.Instance);
 			}
-			if (dClicker.currentSkill != null) {
-				dClicker.AbortSkill();
-			}
-			this.Dialog(ac, SingletonScript<Dialogs.D_RegBox>.Instance);
 		}
 
 	}
@@ -52,7 +51,13 @@ namespace SteamEngine.CompiledScripts.Dialogs {
 
 	[Remark("Surprisingly the dialog that will display the RegBox guts")]
 	public class D_RegBox : CompiledGump {
+
+		private readonly TagKey tkButtonsForReags = TagKey.Get("_rb_ButtonsForReags_");
+		private readonly TagKey tkButtonsCount = TagKey.Get("_rb_ButtonsCount_");
+		
 		public override void Construct(Thing focus, AbstractCharacter sendTo, object[] sa) {
+			Dictionary<int, ItemDef> dictButtonForReags = new Dictionary<int,ItemDef>();
+			int buttonsCount = 0;
 			int i = 0;
 			int radku = 0;
 			RegBox box = (RegBox)focus;
@@ -63,6 +68,9 @@ namespace SteamEngine.CompiledScripts.Dialogs {
 					radku++;
 					i = 0;
 				}
+			}
+			if ((i == 0) && (radku > 0)) { 
+				radku--;
 			}
 			int baseX = 20;
 			int baseY = 60;
@@ -78,15 +86,17 @@ namespace SteamEngine.CompiledScripts.Dialogs {
 			} else {
 				i = 0;
 				foreach (KeyValuePair<ItemDef, int> pair in box.inBoxReags) {
-					Button(baseX, baseY, 4017, 4019, true, 0, 1000 + i);
+					Button(baseX, baseY, 4017, 4019, true, 0, 1000 + buttonsCount);
 					HTMLGumpA(baseX + 35, baseY, 110, 20, pair.Key.Name, false, false);
 					HTMLGumpA(baseX + 35, baseY + 20, 100, 20, "Pocet:", false, false);
 					HTMLGumpA(baseX + 75, baseY + 20, 100, 20, Convert.ToString(pair.Value), false, false);
-					CheckBox(baseX, baseY + 38, 9903, 9904, false, i);
+					CheckBox(baseX, baseY + 38, 9903, 9904, false, buttonsCount);
 					HTMLGumpA(baseX + 35, baseY + 38, 50, 20, "Vyndat:", false, false);
-					NumberEntryA(baseX + 80, baseY + 38, 65, 20, 0, i, 0);
+					NumberEntryA(baseX + 80, baseY + 38, 65, 20, 0, buttonsCount, 0);
 					TilePic(baseX + 110, baseY, pair.Key.Model);
+					dictButtonForReags.Add(buttonsCount, pair.Key);
 					i++;
+					buttonsCount++;
 					if (i < 4) {
 						baseX += 157;
 					} else {
@@ -96,35 +106,52 @@ namespace SteamEngine.CompiledScripts.Dialogs {
 					}
 				}
 			}
+			this.GumpInstance.SetTag(tkButtonsCount, buttonsCount);
+			this.GumpInstance.SetTag(tkButtonsForReags, dictButtonForReags);
 			Button(20, 125 + radku * 80, 4023, 4025, true, 0, 2);		// OK
 		}
 
 		public override void OnResponse(GumpInstance gi, GumpResponse gr, object[] args) {
 			RegBox box = (RegBox)gi.Focus;
+			if (!((Player)gi.Cont).CanReach(box)) {
+				((Player)gi.Cont).SysMessage("Jsi pøíliš daleko.");
+				return;
+			}
 			if (gr.pressedButton == 0) {			// cancel
 				return;
 			} else if (gr.pressedButton == 1) {		// Add reags
 				((Player) gi.Cont).Target(SingletonScript<Targ_RegBox>.Instance, gi.Focus);
 			} else if (gr.pressedButton == 2) {		// OK -> give selected reags
+				Dictionary<int, ItemDef> buttonShowItemDef = (Dictionary<int, ItemDef>)gi.GetTag(tkButtonsForReags);
+				int buttonsCount = (int)gi.GetTag(tkButtonsCount);
 				int i = 0;
 				int reagsToGive = 0;
-				foreach (KeyValuePair<ItemDef, int> pair in box.inBoxReags) {
+				while (i < buttonsCount) {
 					if ( (gr.IsSwitched(i)) && (gr.responseNumbers[i].number > 0) ){	// player wants to take at least one reagent
-						if (pair.Value < (int)gr.responseNumbers[i].number) {
-							((Player)gi.Cont).RedMessage("Snazis se vyndat prilis mnoho regu: " + pair.Key.Name + ". Vyndavas jen tolik, kolik muzes.");
-							reagsToGive = pair.Value;
+						if (box.inBoxReags[buttonShowItemDef[i]] < (int)gr.responseNumbers[i].number) {
+							((Player)gi.Cont).RedMessage("Snazis se vyndat prilis mnoho regu: " + buttonShowItemDef[i].Name + ". Vyndavas jen tolik, kolik muzes.");
+							reagsToGive = box.inBoxReags[buttonShowItemDef[i]];
 						} else {
 							reagsToGive = (int)gr.responseNumbers[i].number;
 						}
-						pair.Key.Create(((Player)gi.Cont).Backpack);
+						buttonShowItemDef[i].Create(((Player)gi.Cont).Backpack);
 						Globals.lastNewItem.Amount = (uint)reagsToGive;
-						gi.Cont.Message("vyndavam: "+pair.Key.Name);
-						box.inBoxReags[pair.Key] -= reagsToGive;
+						gi.Cont.SysMessage("Vyndáváš z bedny " + Convert.ToString(reagsToGive) + "ks regu " + buttonShowItemDef[i].Name + ".");
+						box.inBoxReags[buttonShowItemDef[i]] -= reagsToGive;
+						box.pocetRegu -= reagsToGive;
+						if (box.inBoxReags[buttonShowItemDef[i]] == 0) {
+							box.inBoxReags.Remove(buttonShowItemDef[i]);
+						}
 					}
 					i++;
 				}
-
-				
+			} else if (gr.pressedButton >= 1000) {
+				int thisButtonValue = (int)gr.pressedButton - 1000;
+				Dictionary<int, ItemDef> buttonShowItemDef = (Dictionary<int, ItemDef>)gi.GetTag(tkButtonsForReags);
+				buttonShowItemDef[thisButtonValue].Create(((Player)gi.Cont).Backpack);
+				Globals.lastNewItem.Amount = (uint)box.inBoxReags[buttonShowItemDef[thisButtonValue]];
+				box.inBoxReags.Remove(buttonShowItemDef[thisButtonValue]);
+				box.Dialog(gi.Cont, SingletonScript<Dialogs.D_RegBox>.Instance);
 			}
 		}
 	}
@@ -138,11 +165,8 @@ namespace SteamEngine.CompiledScripts.Dialogs {
 
 		protected override bool On_TargonItem(Character self, Item targetted, object parameter) {
 			RegBox focus = parameter as RegBox;
-			if (!self.GetMap().CanSeeLOSFromTo(self, targetted)) {
-				self.ClilocSysMessage(500876);
-			}
-			if ((Point2D.GetSimpleDistance(self, focus) > 3) || (Point2D.GetSimpleDistance(self, targetted) > 3)) {
-				self.ClilocSysMessage(500295);
+			if ( (!self.CanReach(focus)) || (!self.CanReach(targetted)) ) {
+				self.SysMessage("Jsi pøíliš daleko.");
 				return false;
 			}
 			if (targetted.Type.Defname == "t_reagent") {
