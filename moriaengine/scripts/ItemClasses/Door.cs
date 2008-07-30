b10b01f7e1fd425bcbd267d86a69a646b932671b
Rao <Rao@6780152c-57a2-4364-a193-6a9102f7105b>
2008-07-30 22:52:20 +0000

    A znova dvere. Upraveno snad vsechno + prodano nekolik dalsich typu dveri.

--- moriaengine/scripts/ItemClasses/Door.cs
+++ moriaengine/scripts/ItemClasses/Door.cs
@@ -23,304 +23,197 @@ namespace SteamEngine.CompiledScripts {
     [Dialogs.ViewableClass]
     public partial class Door : Item {
 
-        public bool opened;
-        private ushort rd = 16;
-
-        private void Set() {
-            DoorType dt = this.Info();
-            rd = (ushort)(this.Model - dt.baseType);        //Stands for relative position of graphic
-            dt = this.Info();
-            this.opened = dt.opened;
-        }
-
-        public static bool IsOpen(Door door) {
-            if (door.opened == true) {
-                return true;
-            } else {
-                return false;
-            }
-        }
+        private ushort rd = 0;
+        ushort[] bType = new ushort[] {
+            0xE8,     //secret stone door		
+            0x314,    //secret stone door
+            0x324,    //secret stone door
+            0x334,    //secret wooden door - chestnut
+            0x344,    //secret wooden door - spruce
+            0x354,    //secret stone door
+            0x675,    //iron door
+            0x685,    //iron bar door
+            0x695,    //rattan door
+            0x6a5,    //dark wooden door
+            0x6b5,    //wooden door
+            0x6c5,    //iron door
+            0x6d5,    //light wooden door
+            0x6e5,    //dark wooden door with iron
+            0x824,    //iron bar gate external
+            0x839,    //light wooden short door
+            0x84c,    //iron bar gate external
+            0x866,    //dark wooden short door
+            0x1fed,   //barred metal door
+            0x190e    //bar door - retarded doors, only 2 grafics. needs to stay last
+        };
 
         private void Validity() {
-            DoorType dt = this.Info();
-            if (rd != (this.Model - dt.baseType)) {
-                throw new SEException("rd of doors is invalid.");
+            if (this.GetBaseType() == 0x190e) {
+                if (!((rd == 1) || (rd == 0))) {
+                    throw new SEException("rd of retarded BAR doors is out of range.");
+                } else {
+                    rd = (ushort)(this.Model - this.GetBaseType());
+                }
             }
-            if (this.opened != dt.opened) {
-                throw new SEException("opened of doors is invalid.");
+            if (rd != (this.Model - this.GetBaseType())) {
+                if ((rd > 15) || (rd < 0)) {
+                    throw new SEException("rd of doors is out of range.");
+                } else {
+                    rd = (ushort)(this.Model - this.GetBaseType());
+                }
             }
         }
 
         public override void On_DClick(AbstractCharacter user) {
-            if (rd == 16) {        //Does only on first time of use
-                this.Set();
+            this.Validity();
+            if (!this.IsOpen()) {
+                Trigger_Open();
             } else {
-                this.Validity();
+                Trigger_Close();
             }
-            Character person = user as Character;
-            if (person.CanReach(this) == DenyResult.Allow) {
-                if (this.opened == false) {
-                    Open(person);
-                } else {
-                    Close(person);
+        }
+
+        public void Trigger_Open() {
+            On_DenyOpen();
+            On_Open();
+            SetOpen();
+        }
+
+        public void Trigger_Close() {
+            On_DenyClose();
+            On_Close();
+            SetClose();
+        }
+
+        public void On_DenyOpen() { }
+
+        public void On_Open() { }
+
+        public void On_DenyClose() { }
+
+        public void On_Close() { }
+
+        public void SetOpen() {
+            if (!this.IsOpen()) {           //For case of opening opened doors.
+                switch (rd) {
+                    case 0:
+                        this.X--;
+                        this.Y++;
+                        break;
+                    case 2:
+                        this.X++;
+                        this.Y++;
+                        break;
+                    case 4:
+                        this.X--;
+                        break;
+                    case 6:
+                        this.X++;
+                        this.Y--;
+                        break;
+                    case 8:
+                        this.X++;
+                        this.Y++;
+                        break;
+                    case 10:
+                        this.X++;
+                        this.Y--;
+                        break;
+                    case 12:
+                        break;
+                    case 14:
+                        this.Y--;
+                        break;
                 }
+                this.Model++;
+                rd++;
             }
         }
 
-        public void Open(Character person) {
-            this.opened = true;
-            switch (rd) {
-                case 0:
-                    this.X = (ushort)(this.X - 1);
-                    this.Y = (ushort)(this.Y + 1);
-                    break;
-                case 2:
-                    this.X = (ushort)(this.X + 1);
-                    this.Y = (ushort)(this.Y + 1);
-                    break;
-                case 4:
-                    this.X = (ushort)(this.X - 1);
-                    //this.Y = (ushort)(this.Y - 1);
-                    break;
-                case 6:
-                    this.X = (ushort)(this.X + 1);
-                    this.Y = (ushort)(this.Y - 1);
-                    break;
-                case 8:
-                    this.X = (ushort)(this.X + 1);
-                    this.Y = (ushort)(this.Y + 1);
-                    break;
-                case 10:
-                    this.X = (ushort)(this.X + 1);
-                    this.Y = (ushort)(this.Y - 1);
-                    break;
-                case 12:
-                    //this.X = (ushort)(this.X - 1);
-                    //this.Y = (ushort)(this.Y + 1);
-                    break;
-                case 14:
-                    //this.X = (ushort)(this.X - 1);
-                    this.Y = (ushort)(this.Y - 1);
-                    break;
-            }
-            this.Model = (ushort)(this.Model + 1);
-            rd = (ushort)(rd + 1);
-        }
-
-        public void Close(Character person) {
-            this.opened = false;
-            switch (rd) {
-                case 1:
-                    this.X = (ushort)(this.X + 1);
-                    this.Y = (ushort)(this.Y - 1);
-                    break;
-                case 3:
-                    this.X = (ushort)(this.X - 1);
-                    this.Y = (ushort)(this.Y - 1);
-                    break;
-                case 5:
-                    this.X = (ushort)(this.X + 1);
-                    //this.Y = (ushort)(this.Y + 1);
-                    break;
-                case 7:
-                    this.X = (ushort)(this.X - 1);
-                    this.Y = (ushort)(this.Y + 1);
-                    break;
-                case 9:
-                    this.X = (ushort)(this.X - 1);
-                    this.Y = (ushort)(this.Y - 1);
-                    break;
-                case 11:
-                    this.X = (ushort)(this.X - 1);
-                    this.Y = (ushort)(this.Y + 1);
-                    break;
-                case 13:
-                    //this.X = (ushort)(this.X + 1);
-                    //this.Y = (ushort)(this.Y - 1);
-                    break;
-                case 15:
-                    //this.X = (ushort)(this.X + 1);
-                    this.Y = (ushort)(this.Y + 1);
-                    break;
-            }
-            this.Model = (ushort)(this.Model - 1);
-            rd = (ushort)(rd - 1);
-        }
-
-        [Summary("Method returning info about doors. E.G. opened, direction, description...")]
-        public DoorType Info() {
-            ushort[] bType = new ushort[12];
-            bType[0] = 0x675;    //iron door
-            bType[1] = 0x685;    //iron bar door
-            bType[2] = 0x695;    //rattan door
-            bType[3] = 0x6a5;    //dark wooden door
-            bType[4] = 0x6b5;    //wooden dor
-            bType[5] = 0x6c5;    //iron door
-            bType[6] = 0x6d5;    //light wooden door
-            bType[7] = 0x6e5;    //dark wooden door with iron
-            bType[8] = 0x824;    //iron bar gate external
-            bType[9] = 0x839;    //light wooden short door
-            bType[10] = 0x84c;   //iron bar gate external
-            bType[11] = 0x866;   //dark wooden short door
-            ushort grafic = this.Model;
-            DoorType dType = new DoorType();
-
-            for (int i = 0; i < 12; i++) {
-                if ((grafic >= bType[i]) && (grafic <= (bType[i] + 15))) {
-                    dType.baseType = bType[i];
-                    break;
+        public void SetClose() {
+            if (this.IsOpen()) {            //For case of closing closed doors.
+                switch (rd) {
+                    case 1:
+                        this.X++;
+                        this.Y--;
+                        break;
+                    case 3:
+                        this.X--;
+                        this.Y--;
+                        break;
+                    case 5:
+                        this.X++;
+                        break;
+                    case 7:
+                        this.X--;
+                        this.Y++;
+                        break;
+                    case 9:
+                        this.X--;
+                        this.Y--;
+                        break;
+                    case 11:
+                        this.X--;
+                        this.Y++;
+                        break;
+                    case 13:
+                        break;
+                    case 15:
+                        this.Y++;
+                        break;
                 }
+                this.Model--;
+                rd--;
             }
-            rd = (ushort)(this.Model - dType.baseType);
-            return this.RotationAndDirection(dType);
-
-            /*
-            if (grafic >= 0x675 && grafic <= 0x684) {
-                dType.baseType = 0x675;
-                dType.description = "iron door";
-            } else if (grafic >= 0x685 && grafic <= 0x694) {
-                dType.baseType = 0x685;
-                dType.description = "iron bar door";
-            } else if (grafic >= 0x695 && grafic <= 0x6a4) {
-                dType.baseType = 0x695;
-                dType.description = "rattan door";
-            } else if (grafic >= 0x6a5 && grafic <= 0x6b4) {
-                dType.baseType = 0x6a5;
-                dType.description = "dark wooden door";
-            } else if (grafic >= 0x6b5 && grafic <= 0x6c4) {
-                dType.baseType = 0x6b5;
-                dType.description = "wooden dor";
-            } else if (grafic >= 0x6c5 && grafic <= 0x6d4) {
-                dType.baseType = 0x6c5;
-                dType.description = "iron door";
-            } else if (grafic >= 0x6d5 && grafic <= 0x6e4) {
-                dType.baseType = 0x6d5;
-                dType.description = "light wooden door";
-            } else if (grafic >= 0x6e5 && grafic <= 0x6f4) {
-                dType.baseType = 0x6e5;
-                dType.description = "dark wooden door with iron";
-            } else if (grafic >= 0x824 && grafic <= 0x833) {
-                dType.baseType = 0x824;
-                dType.description = "iron bar gate external";
-            } else if (grafic >= 0x839 && grafic <= 0x848) {
-                dType.baseType = 0x839;
-                dType.description = "light wooden short door";
-            } else if (grafic >= 0x84c && grafic <= 0x85b) {
-                dType.baseType = 0x84c;
-                dType.description = "iron bar gate external";
-            } else if (grafic >= 0x866 && grafic <= 0x875) {
-                dType.baseType = 0x866;
-                dType.description = "dark wooden short door";
-            } else {
-                throw new SEException("This isn´t a door.");
-            }*/
         }
 
-        private DoorType RotationAndDirection(DoorType dt) {
-            if ((rd % 2) == 0) {
-                dt.opened = false;
-            } else {
-                dt.opened = true;
-            }
-           // dt.opened = ((rd % 2) == 1);
-            if (((rd / 2) % 2) == 0) {
-                dt.rotation = Rotation.Left;
-            } else {
-                dt.rotation = Rotation.Right;
-            }
+        [Summary("Is doors orthogonal(kolmé) on which direction?")]
+        public Direction GetDirection() {
+            this.Validity();
             switch (rd / 4) {
                 case 0:
-                    dt.direction = Direction.North;
-                    break;
+                    return Direction.North;
                 case 1:
-                    dt.direction = Direction.South;
-                    break;
+                    return Direction.South;
                 case 2:
-                    dt.direction = Direction.West;
-                    break;
+                    return Direction.West;
                 case 3:
-                    dt.direction = Direction.East;
-                    break;
+                    return Direction.East;
             }
-            return dt;
+            throw new SEException("Something is wrong, maybe it isnt a door?");
+        }
 
-            /* if (rd == 0) {
-                 dt.direction = Direction.North;
-                 dt.opened = false;
-                 dt.rotation = Rotation.Left;
-             } else if (rd == 1) {
-                 dt.direction = Direction.North;
-                 dt.opened = true;
-                 dt.rotation = Rotation.Left;
-             } else if (rd == 2) {
-                 dt.direction = Direction.North;
-                 dt.opened = false;
-                 dt.rotation = Rotation.Right;
-             } else if (rd == 3) {
-                 dt.direction = Direction.North;
-                 dt.opened = true;
-                 dt.rotation = Rotation.Right;
-             } else if (rd == 4) {
-                 dt.direction = Direction.South;
-                 dt.opened = false;
-                 dt.rotation = Rotation.Left;
-             } else if (rd == 5) {
-                 dt.direction = Direction.South;
-                 dt.opened = true;
-                 dt.rotation = Rotation.Left;
-             } else if (rd == 6) {
-                 dt.direction = Direction.South;
-                 dt.opened = false;
-                 dt.rotation = Rotation.Right;
-             } else if (rd == 7) {
-                 dt.direction = Direction.South;
-                 dt.opened = true;
-                 dt.rotation = Rotation.Right;
-             } else if (rd == 8) {
-                 dt.direction = Direction.West;
-                 dt.opened = false;
-                 dt.rotation = Rotation.Left;
-             } else if (rd == 9) {
-                 dt.direction = Direction.West;
-                 dt.opened = true;
-                 dt.rotation = Rotation.Left;
-             } else if (rd == 10) {
-                 dt.direction = Direction.West;
-                 dt.opened = false;
-                 dt.rotation = Rotation.Right;
-             } else if (rd == 11) {
-                 dt.direction = Direction.West;
-                 dt.opened = true;
-                 dt.rotation = Rotation.Right;
-             } else if (rd == 12) {
-                 dt.direction = Direction.East;
-                 dt.opened = false;
-                 dt.rotation = Rotation.Left;
-             } else if (rd == 13) {
-                 dt.direction = Direction.East;
-                 dt.opened = true;
-                 dt.rotation = Rotation.Left;
-             } else if (rd == 14) {
-                 dt.direction = Direction.East;
-                 dt.opened = false;
-                 dt.rotation = Rotation.Right;
-             } else if (rd == 15) {
-                 dt.direction = Direction.East;
-                 dt.opened = true;
-                 dt.rotation = Rotation.Right;
-             }*/
+        public bool IsOpen() {
+            this.Validity();
+            if ((rd % 2) == 0) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+
+        public Rotation GetRotation() {
+            this.Validity();
+            if (((rd / 2) % 2) == 0) {
+                return Rotation.Left;
+            } else {
+                return Rotation.Right;
+            }
+        }
+
+        public ushort GetBaseType() {       //This metod doesnt need (no use of rd) and CANNOT call Validity method (infinite loop)
+            ushort grafic = this.Model;
+            if ((grafic == 0x190e) || (grafic == 0x190f)) {     //bar door.
+                return 0x190e;
+                for (int i = 0; i < 19; i++) {      //20 doors (0-19) but the last door are retarded bar door.
+                    if ((grafic >= bType[i]) && (grafic <= (bType[i] + 15))) {
+                        return bType[i];
+                    }
+                }
+                throw new SEException("This isnt a door perhaps.");
+            }
         }
     }
-
-    [Summary("Return type, containing info about doors. E.G. opened, direction, description...")]
-    public class DoorType {
-        public bool opened;
-        public Direction direction;
-        public Rotation rotation;
-        public ushort baseType;
-        //public string description;
-    }
-
     [Summary("Does doors rotate by left or right side?")]
     public enum Rotation {
         Left,
