64273fdfb3c514f5d3af8c19a3c66f38b1fcbd76
biosek <biosek@6780152c-57a2-4364-a193-6a9102f7105b>
2010-02-04 22:42:13 +0000

    Do projektu pridana slozka pro taleny clerica, trida pro selfress, script pro Selfress.cs TODO: v selfressu dodelat resource svicky a sanci pro povedeni selfressu

---
+++ moriaengine/scripts/systems/Abilities/Talents/Cleric/Selfress.cs
@@ -0,0 +1,50 @@
+/*
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+	Or visit http://www.gnu.org/copyleft/gpl.html
+*/
+
+using System;
+using System.Reflection;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using SteamEngine.Common;
+using SteamEngine.CompiledScripts.Dialogs;
+
+namespace SteamEngine.CompiledScripts {
+    [Summary("Cleric's selfress")]
+    [ViewableClass]
+    public class SelfressDef : ImmediateAbilityDef {
+        //TODO: resource -> newbie svicka potrebna pro samoress, sance?
+        public SelfressDef(string defname, string filename, int headerLine)
+            : base(defname, filename, headerLine) {
+        }
+
+        #region triggerMethods
+        public override void Activate(Character chr) {
+            if (!chr.IsAliveAndValid) {
+                base.Activate(chr);
+            } else {
+                chr.SysMessage("Nejsi mrtvý, nemùžeš se vzkøísit!");
+            }
+        }
+
+        protected override bool On_Activate(Character chr, Ability ab) {
+            chr.Resurrect();
+            return false;
+        }
+        #endregion triggerMethods
+    }
+}
