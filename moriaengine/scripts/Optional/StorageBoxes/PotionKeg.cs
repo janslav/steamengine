c90ee4d905106a90a2ff54aa4becfb3a00a3b49a
tartaros <tartaros@6780152c-57a2-4364-a193-6a9102f7105b>
2011-11-28 05:11:27 +0000

    soubor reformatovan a odstranen warning. Mily autor bude tak laskav a nastavi si formatovani ve visual studiu :)

--- moriaengine/scripts/Optional/StorageBoxes/PotionKeg.cs
+++ moriaengine/scripts/Optional/StorageBoxes/PotionKeg.cs
@@ -15,12 +15,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 Or visit http://www.gnu.org/copyleft/gpl.html
 */
 
-using SteamEngine;
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using SteamEngine.Common;
-using SteamEngine.LScript;
 using SteamEngine.Common;
 using SteamEngine.Communication.TCP;
 using SteamEngine.Networking;
@@ -30,9 +25,9 @@ namespace SteamEngine.CompiledScripts {
 	[Dialogs.ViewableClass]
 	public partial class PotionKeg {
 
-		public override void On_DClick (AbstractCharacter ac) {
-			((Player)ac).Target(SingletonScript<Targ_PotionKeg>.Instance, this);
-			base.On_DClick (ac);
+		public override void On_DClick(AbstractCharacter ac) {
+			((Player) ac).Target(SingletonScript<Targ_PotionKeg>.Instance, this);
+			base.On_DClick(ac);
 		}
 
 		public override void On_Click(AbstractCharacter clicker, GameState clickerState, TcpConnection<GameState> clickerConn) {
@@ -42,59 +37,59 @@ namespace SteamEngine.CompiledScripts {
 				String.Concat(this.potionsCount.ToString(), " potions"),
 				0);
 		}
-
 	}
-	public class Targ_PotionKeg: CompiledTargetDef {
 
-		protected override void On_Start (Player self, object parameter) {
-			self.SysMessage ("Zaměř potiony, které chceš vylít do kegu");
-			base.On_Start (self, parameter);
+	public class Targ_PotionKeg : CompiledTargetDef {
+
+		protected override void On_Start(Player self, object parameter) {
+			self.SysMessage("Zaměř potiony, které chceš vylít do kegu");
+			base.On_Start(self, parameter);
 		}
 
 		protected override TargetResult On_TargonItem(Player self, Item targetted, object parameter) {
-			PotionKeg keg = (PotionKeg)parameter;
+			PotionKeg keg = (PotionKeg) parameter;
 			Potion potion = targetted as Potion;
-			
-			if (!self.CanReachWithMessage (keg)) {
-				self.SysMessage ("CanReachWithMessage");
+
+			if (!self.CanReachWithMessage(keg)) {
+				self.SysMessage("CanReachWithMessage");
 				return TargetResult.RestartTargetting;
 			}
 			if (potion != null) {
 				if (keg.potionDef == null) {
-				    keg.potionDef = potion.TypeDef;
-				    keg.Color = potion.Color;
+					keg.potionDef = potion.TypeDef;
+					keg.Color = potion.Color;
 				}
-				
+
 				if (keg.potionDef == potion.TypeDef) {
-				    ThingDef.GetByDefname("i_bottle_empty").Create(((Player)self).Backpack);
-				    if ((keg.TypeDef.Capacity - keg.potionsCount) < targetted.Amount) {	// poresime prekroceni nosnosti kegu -> do kegu se prida jen tolik potionu, kolik skutecne lze pridat
-				        int potionsToTake = keg.TypeDef.Capacity - keg.potionsCount;
-				        targetted.Amount -= potionsToTake;
-				        keg.potionsCount += potionsToTake;
-				        Globals.LastNewItem.Amount = potionsToTake;
-				    } else {
-				        keg.potionsCount += targetted.Amount;
-				        potion.Delete();
-				        Globals.LastNewItem.Amount = targetted.Amount;
-				    }
+					ThingDef.GetByDefname("i_bottle_empty").Create(((Player) self).Backpack);
+					if ((keg.TypeDef.Capacity - keg.potionsCount) < targetted.Amount) {	// poresime prekroceni nosnosti kegu -> do kegu se prida jen tolik potionu, kolik skutecne lze pridat
+						int potionsToTake = keg.TypeDef.Capacity - keg.potionsCount;
+						targetted.Amount -= potionsToTake;
+						keg.potionsCount += potionsToTake;
+						Globals.LastNewItem.Amount = potionsToTake;
+					} else {
+						keg.potionsCount += targetted.Amount;
+						potion.Delete();
+						Globals.LastNewItem.Amount = targetted.Amount;
+					}
 				} else {
-				    self.SysMessage("Tim bys to celé skazil!");
+					self.SysMessage("Tim bys to celé skazil!");
 				}
-			} else if(targetted.Type.Defname == "t_bottle_empty"){
-			    if (keg.potionDef != null) {
-			        if (targetted.Amount < keg.potionsCount) {
-			            keg.potionDef.Create(((Player)self).Backpack);
-			            Globals.LastNewItem.Amount = targetted.Amount;
-			            keg.potionsCount -= targetted.Amount;
-			            targetted.Delete();
-			        } else {
-			            keg.potionDef.Create(((Player)self).Backpack);
-			            Globals.LastNewItem.Amount = keg.potionsCount;
-						
+			} else if (targetted.Type.Defname == "t_bottle_empty") {
+				if (keg.potionDef != null) {
+					if (targetted.Amount < keg.potionsCount) {
+						keg.potionDef.Create(((Player) self).Backpack);
+						Globals.LastNewItem.Amount = targetted.Amount;
+						keg.potionsCount -= targetted.Amount;
+						targetted.Delete();
+					} else {
+						keg.potionDef.Create(((Player) self).Backpack);
+						Globals.LastNewItem.Amount = keg.potionsCount;
+
 						if (targetted.Amount == keg.potionsCount) {
-						    targetted.Delete();
+							targetted.Delete();
 						} else {
-						    targetted.Amount -= keg.potionsCount;
+							targetted.Amount -= keg.potionsCount;
 						}
 
 						keg.Color = 0;
@@ -103,9 +98,9 @@ namespace SteamEngine.CompiledScripts {
 					}
 				}
 			} else {
-				self.SysMessage ("Můžeš nalít jenom potiony.");
+				self.SysMessage("Můžeš nalít jenom potiony.");
 			}
-         
+
 			return TargetResult.Done;
 		}
 
