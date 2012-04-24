7d0c96f12c1e25a2d8b618a6d5cdd78cd8c6577d
tartaros <janslav@users.noreply.github.com>
2012-04-24 00:18:58 +0000

    piseme pismenka do "logu"! :)

---
+++ Cruncher/CrunchyUtils/Logger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CrunchyUtils
+{
+    public static class Logger
+    {
+		public static void Write(object msg) {
+
+			var w = Application.Current.MainWindow;
+
+			var tbLog = (RichTextBox) w.FindName("tbLog");
+
+			tbLog.AppendText(string.Concat(msg, Environment.NewLine));
+		}
+
+    }
+}
