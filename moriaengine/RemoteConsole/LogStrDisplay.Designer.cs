ccf3ef02a73453de591bef708ac7abcff4138c45
tart <tart@6780152c-57a2-4364-a193-6a9102f7105b>
2016-04-08 19:07:13 +0000

    text fix

--- moriaengine/RemoteConsole/LogStrDisplay.Designer.cs
+++ moriaengine/RemoteConsole/LogStrDisplay.Designer.cs
@@ -24,7 +24,7 @@ namespace SteamEngine.RemoteConsole {
 		/// </summary>
 		private void InitializeComponent() {
 			this.chckAutoScroll = new System.Windows.Forms.CheckBox();
-			this.chkContract = new System.Windows.Forms.CheckBox();
+			this.chkCompress = new System.Windows.Forms.CheckBox();
 			this.btnClear = new System.Windows.Forms.Button();
 			this.txtBox = new SteamEngine.RemoteConsole.ExtendedRichTextBox();
 			this.SuspendLayout();
@@ -41,17 +41,17 @@ namespace SteamEngine.RemoteConsole {
 			this.chckAutoScroll.Text = "Autoscroll";
 			this.chckAutoScroll.UseVisualStyleBackColor = true;
 			// 
-			// chkContract
+			// chkCompress
 			// 
-			this.chkContract.AutoSize = true;
-			this.chkContract.Checked = true;
-			this.chkContract.CheckState = System.Windows.Forms.CheckState.Checked;
-			this.chkContract.Location = new System.Drawing.Point(81, 7);
-			this.chkContract.Name = "chkContract";
-			this.chkContract.Size = new System.Drawing.Size(140, 17);
-			this.chkContract.TabIndex = 1;
-			this.chkContract.Text = "Contract multiline entries";
-			this.chkContract.UseVisualStyleBackColor = true;
+			this.chkCompress.AutoSize = true;
+			this.chkCompress.Checked = true;
+			this.chkCompress.CheckState = System.Windows.Forms.CheckState.Checked;
+			this.chkCompress.Location = new System.Drawing.Point(81, 7);
+			this.chkCompress.Name = "chkCompress";
+			this.chkCompress.Size = new System.Drawing.Size(140, 17);
+			this.chkCompress.TabIndex = 1;
+			this.chkCompress.Text = "Compress multiline entries";
+			this.chkCompress.UseVisualStyleBackColor = true;
 			// 
 			// btnClear
 			// 
@@ -82,7 +82,7 @@ namespace SteamEngine.RemoteConsole {
 			// LogStrDisplay
 			// 
 			this.Controls.Add(this.btnClear);
-			this.Controls.Add(this.chkContract);
+			this.Controls.Add(this.chkCompress);
 			this.Controls.Add(this.chckAutoScroll);
 			this.Controls.Add(this.txtBox);
 			this.Name = "LogStrDisplay";
@@ -96,7 +96,7 @@ namespace SteamEngine.RemoteConsole {
 
 		private ExtendedRichTextBox txtBox;
 		private System.Windows.Forms.CheckBox chckAutoScroll;
-		private System.Windows.Forms.CheckBox chkContract;
+		private System.Windows.Forms.CheckBox chkCompress;
 		private System.Windows.Forms.Button btnClear;
 
 	}
