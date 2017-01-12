480eabf459b3fb3c2481100f578791a72bdc4dc2
tart <janslav@users.noreply.github.com>
2017-01-12 01:13:48 +0000

    Adding Shielded library. It's a nuget but is 32-bit for some reason, so had to build it myself andput the binaries in place of the nuget ones, for now.

---
+++ moriaengine/Tests/TransactionTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shielded;
+
+namespace SteamEngine.Tests {
+	[TestClass]
+	public class TransactionTests {
+
+		[TestMethod]
+		public void Trivial() {
+			Shield.InTransaction(() => {
+
+
+			});
+		}
+	}
+}
