65c7cb525cde345fea8fd89d42adc3a770ce51b3
janslav <jslavetinsky@seznam.cz>
2017-01-27 01:49:07 +0100

    SeShield fixed

--- GameCore/SeShield.cs
+++ GameCore/SeShield.cs
@@ -8,21 +8,21 @@ using Shielded;
 namespace SteamEngine {
 	public static class SeShield {
 		public static void AssertNotInTransaction() {
-			if (SeShield.IsInTransaction) {
+			if (Shield.IsInTransaction) {
 				throw new InvalidOperationException("Operation must not to be in a transaction.");
 			}
 		}
 
 		public static void AssertInTransaction() {
-			SeShield.AssertInTransaction();
+			Shield.AssertInTransaction();
 		}
 
 		public static T InTransaction<T>(Func<T> act) {
-			return SeShield.InTransaction(act);
+			return Shield.InTransaction(act);
 		}
 
 		public static void InTransaction(Action act) {
-			SeShield.InTransaction(act);
+			Shield.InTransaction(act);
 		}
 	}
 }
